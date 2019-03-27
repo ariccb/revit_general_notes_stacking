b0797ba897c542a06dd6c5fbf9fd7202fe1b5c06
chrisfebbraro <35147736+chrisfebbraro@users.noreply.github.com>
2019-03-27 07:18:12 -0400

    added line to test pull requests

--- rjc.GeneralNotesAutomation/FormatGeneralNote.cs
+++ rjc.GeneralNotesAutomation/FormatGeneralNote.cs
@@ -15,6 +15,8 @@ namespace rjc.GeneralNotesAutomation
     [Regeneration(RegenerationOption.Manual)]
     class FormatGeneralNote
     {
+
+        //adding this line to test pull requests
         public void MoveNoteToViewOrigin(Autodesk.Revit.DB.Document doc)
         {
             //UIApplication uiApp = commandData.Application;
